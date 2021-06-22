f53d34b3ad4a935c3f91e9952b44890f65a853cb
MT <littlehappytroll@gmail.com>
2021-06-22 01:57:43 -0400

    pog

---
+++ SlicePause/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Loader;
+using IPALogger = IPA.Logging.Logger;
+using System.Net.Http;
+using SlicePause.HarmonyPatches;
+using SiraUtil.Zenject;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SlicePause
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        public static readonly string HarmonyId = "com.github.Goobwabber.SlicePause";
+
+        internal static Plugin Instance { get; private set; } = null!;
+        internal static PluginMetadata PluginMetadata = null!;
+        internal static IPALogger Log { get; private set; } = null!;
+
+        internal static HttpClient HttpClient { get; private set; } = null!;
+        internal static Harmony? _harmony;
+        internal static Harmony Harmony
+        {
+            get
+            {
+                return _harmony ??= new Harmony(HarmonyId);
+            }
+        }
+
+        [Init]
+        public Plugin(IPALogger logger, Zenjector zenjector, PluginMetadata pluginMetadata)
+        {
+            Instance = this;
+            PluginMetadata = pluginMetadata;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            HarmonyManager.ApplyDefaultPatches();
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+    }
+}
